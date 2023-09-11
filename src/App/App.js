9899d4e384a8100a216ea95913feb0dad074be1e
Marisa Wyatt <risawyatt22@gmail.com>
2023-09-11 09:53:03 -0600

    Get current data on page load

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 import React from 'react';
+import { promise } from '../apiCalls';
 
 function App() {
+  let data = promise.then((data) => {
+    console.log('data:', data)
+  })
+
   return (
     <div className="App">
       <h1 className='app-title'>Turing Cafe Reservations</h1>
